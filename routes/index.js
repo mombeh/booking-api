103aefce08ca3f33964c9313135d495c3963a206
mombeh nadine <nadinemombeh@gmail.com>
2025-05-03 21:25:40 +0100

    created a healthy server to handle the engine

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,10 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  return res.json({
+    message: "Sever Is healthy",
+    timeStamp: new Date().toISOString(),
+  });
 });
 
 export default router;
