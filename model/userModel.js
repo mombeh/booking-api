63586fa0f47d41e71aa1da500d119c1620ce6fa8
mombeh <nadinemombeh@gmail.com>
2025-12-20 12:42:16 +0100

    added the role in the user model

--- model/userModel.js
+++ model/userModel.js
@@ -1,16 +1,25 @@
 //model/usermodel
 import { query } from '../config/db.js';
 
-export const createUser = async (firstName, lastName, email, hashedPassword) => {
-    const insertQuery = `
-      INSERT INTO users (first_name, last_name, email, password)
-      VALUES ($1, $2, $3, $4)
-      RETURNING id, first_name, last_name, email, created_at;
-    `;
-    const values = [firstName, lastName, email, hashedPassword];
-    const result = await query(insertQuery, values);
-    return result.rows[0];
-  };
+export const createUser = async (
+  firstName,
+  lastName,
+  email,
+  hashedPassword,
+  role
+) => {
+  const result = await query(
+    `
+    INSERT INTO users (first_name, last_name, email, password, role)
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING id, first_name, last_name, email, role;
+    `,
+    [firstName, lastName, email, hashedPassword, role]
+  );
+
+  return result.rows[0];
+};
+
 
 export const findUserByEmail = async (email) => {
   const result = await query('SELECT * FROM users WHERE email = $1', [email]);
